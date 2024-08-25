9f8cae18501011390c560f8017368bcaff255a9f
sangeeta8642 <sangeetaperagani8642@gmail.com>
2024-08-25 19:56:28 +0530

    1st

---
+++ src/app/api/posts/route.js
@@ -0,0 +1,11 @@
+import { Post } from "@/lib/models";
+import { NextResponse } from "next/server";
+
+export const GET=async (req) => {
+    try {
+        const posts=await Post.find()
+        return NextResponse.json(posts)
+    } catch (error) {
+        console.log(error);
+    }
+}
