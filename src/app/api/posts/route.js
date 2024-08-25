642e4fc7caabd90ab989cd669da366aa715debeb
sangeeta8642 <sangeetaperagani8642@gmail.com>
2024-08-25 21:40:17 +0530

    res

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -6,6 +6,7 @@ export const GET=async (req) => {
         const posts=await Post.find()
         return NextResponse.json(posts)
     } catch (error) {
-        console.log(error);
+        // console.log(error);
+        return NextResponse.json({ error: error.message }, { status: 500 });
     }
 }
